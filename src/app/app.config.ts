2e888b0654f1667ebd8bacab3829b04b625acd30
Lucas Pino <ylucasx3@gmail.com>
2025-05-25 21:58:04 -0300

    fix: update import path for loggingInterceptor to core/interceptors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { loggingInterceptor } from './logging.interceptor';
+import { loggingInterceptor } from './core/interceptors/logging.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
