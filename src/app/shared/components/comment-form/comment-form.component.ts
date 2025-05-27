53a50e39c314d2c60b4b8227cccfdf699d9e323d
Lucas Pino <ylucasx3@gmail.com>
2025-05-26 23:37:24 -0300

    feat: add CommentFormComponent and CommentsListComponent for comment functionality

---
+++ src/app/shared/components/comment-form/comment-form.component.ts
@@ -0,0 +1,60 @@
+import {
+  Component,
+  ChangeDetectionStrategy,
+  EventEmitter,
+  Output,
+  Input,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Comment } from '@app/core/models/comment.model';
+
+@Component({
+  selector: 'app-comment-form',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './comment-form.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CommentFormComponent {
+  @Input({ required: true }) postId!: number;
+  @Output() commentAdded = new EventEmitter<Comment>();
+  commentForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.commentForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      body: ['', Validators.required],
+    });
+  }
+
+  get name() {
+    return this.commentForm.get('name')!;
+  }
+  get email() {
+    return this.commentForm.get('email')!;
+  }
+  get body() {
+    return this.commentForm.get('body')!;
+  }
+
+  onSubmit() {
+    if (this.commentForm.valid) {
+      const newComment: Comment = {
+        postId: this.postId,
+        id: Date.now(),
+        name: this.commentForm.value.name,
+        email: this.commentForm.value.email,
+        body: this.commentForm.value.body,
+      };
+      this.commentAdded.emit(newComment);
+      this.commentForm.reset();
+    }
+  }
+}
---
+++ src/app/shared/components/comments-list/comments-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Comment } from '@app/core/models/comment.model';
+
+@Component({
+  selector: 'app-comments-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './comments-list.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CommentsListComponent {
+  @Input() remoteComments: Comment[] = [];
+  @Input() localComments: Comment[] = [];
+}
