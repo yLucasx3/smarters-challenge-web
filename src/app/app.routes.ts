6e0ecbbcea125d19583a0417eadc66c731309185
Lucas Pino <ylucasx3@gmail.com>
2025-05-26 23:37:44 -0300

    feat: define application routes for posts and post details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/posts', pathMatch: 'full' },
+  {
+    path: 'posts',
+    loadComponent: () =>
+      import('./features/posts/post-feed/post-feed.component').then(
+        (m) => m.PostFeedComponent
+      ),
+  },
+  {
+    path: 'posts/:id',
+    loadComponent: () =>
+      import('./features/posts/post-details/post-details.component').then(
+        (m) => m.PostDetailComponent
+      ),
+  },
+];
