ab72fef64a181118f0fe8ccfca3b4a374c4ffa13
Lucas Pino <ylucasx3@gmail.com>
2025-05-26 23:36:47 -0300

    feat: add PaginationComponent with HTML, CSS, and TypeScript for pagination functionality

---
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -0,0 +1,39 @@
+import {
+  Component,
+  ChangeDetectionStrategy,
+  Input,
+  Output,
+  EventEmitter,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-pagination',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pagination.component.html',
+  styleUrls: ['./pagination.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PaginationComponent {
+  @Input({ required: true }) currentPage!: number;
+  @Input({ required: true }) pageSize!: number;
+  @Input({ required: true }) totalItems!: number;
+  @Output() pageChange = new EventEmitter<number>();
+
+  get totalPages(): number {
+    return Math.ceil(this.totalItems / this.pageSize);
+  }
+
+  previousPage(): void {
+    if (this.currentPage > 1) {
+      this.pageChange.emit(this.currentPage - 1);
+    }
+  }
+
+  nextPage(): void {
+    if (this.currentPage < this.totalPages) {
+      this.pageChange.emit(this.currentPage + 1);
+    }
+  }
+}
