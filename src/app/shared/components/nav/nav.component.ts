0fefc0c41f4aad83aed1180c596af98f490e68cf
Lucas Pino <ylucasx3@gmail.com>
2025-05-26 23:36:23 -0300

    feat: add NavComponent with HTML template and TypeScript for navigation

---
+++ src/app/shared/components/nav/nav.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './nav.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NavComponent {}
